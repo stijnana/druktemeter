3d8c7045362a8cc82681ff3bb0c8908bbb532c10
Stijn Olthof <stijn.olthof@live.nl>
2020-12-18 14:05:47 +0100

    Add files via upload

---
+++ DisplayAndLed.py
@@ -0,0 +1,230 @@
+#import
+import RPi.GPIO as GPIO
+from Adafruit_IO import *
+import struct
+import time
+from time import gmtime, strftime
+import datetime
+
+#set var voor kleur limiten
+greenLed = 10
+blueLed = 20
+redLed = 60
+
+aio = Client('Nizari', 'aio_rsem169oLOMV5K89rjq9Unaut2dB')
+
+locationOne = aio.receive_previous('location')
+
+print(format(locationOne.value))
+
+
+
+def printDateTime():
+  textTime = strftime("%H:%M")
+  print ('time updated')
+  lcd_string(format(locationOne.value),LCD_LINE_1)
+  lcd_string(textTime ,LCD_LINE_2)
+  return
+
+def getInterfaceAddress(ifname):
+  try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+      s.fileno(),
+      0x8915,  # SIOCGIFADDR
+      struct.pack('256s', ifname[:15])
+    )[20:24])
+  except:
+    return ''
+
+
+# Define GPIO to LCD mapping
+LCD_RS = 26
+LCD_E  = 19
+LCD_D4 = 0 #13
+LCD_D5 = 6
+LCD_D6 = 5
+LCD_D7 = 11
+
+RED = 12
+GREEN = 18
+BLUE = 13
+
+# Define some device constants
+LCD_WIDTH = 16    # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+def main():
+  # Main program block
+  
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+  GPIO.setup(LCD_E, GPIO.OUT)  # E
+  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+
+  # Initialise display
+  lcd_init()
+
+  while True:
+    CollectDbData()
+
+    # Display date and time
+    index = 0
+    while index < 10:
+      printDateTime()
+      time.sleep(1)
+      index += 1
+
+def CollectDbData():
+    AveArray = []
+    data = aio.data('sound-levels')
+    CurTime = datetime.datetime.now()
+    SubTime = datetime.timedelta(minutes=65)
+    NewTime = CurTime - SubTime
+    NewTime = NewTime.strftime("%Y-%m-%d %H:%M:%S")
+    NewTime = datetime.datetime.strptime(NewTime, "%Y-%m-%d %H:%M:%S")
+
+    for d in data:
+        SoundVal = format(d.value)
+        TimeVal = format(d.created_at)
+        TimeVal = TimeVal.replace("T", " ")
+        TimeVal = TimeVal.replace("Z", "")
+        TimeVal = datetime.datetime.strptime(TimeVal, "%Y-%m-%d %H:%M:%S")
+        
+        was_date1_before = TimeVal > NewTime
+        
+        if was_date1_before:
+            AveArray.append(SoundVal)
+    for i in range(0, len(AveArray)):
+        AveArray[i] = int(AveArray[i])
+        
+    length = len(AveArray)
+    total = sum(AveArray)
+    averageRound = 0
+    if length > 0:
+        average = total / length
+        averageRound = int(average)
+    
+    print(averageRound)
+    GPIO.setmode(GPIO.BCM)
+    
+    GPIO.setwarnings(False)
+    GPIO.setup(RED,GPIO.OUT)
+    GPIO.output(RED,0)
+    GPIO.setup(GREEN,GPIO.OUT)
+    GPIO.output(GREEN,0)
+    GPIO.setup(BLUE,GPIO.OUT)
+    GPIO.output(BLUE,0)
+    
+    print (averageRound)
+    if averageRound <= greenLed:
+        GPIO.output(RED,100)
+        GPIO.output(GREEN,0)
+        GPIO.output(BLUE,100)
+        
+    if averageRound > greenLed and averageRound <= blueLed:
+        GPIO.output(RED,100)
+        GPIO.output(GREEN,100)
+        GPIO.output(BLUE,0)
+        
+    if averageRound > redLed:
+        GPIO.output(RED,0)
+        GPIO.output(GREEN,100)
+        GPIO.output(BLUE,100)
+
+
+    #time.sleep(60)
+
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = data
+  # mode = True  for character
+  #        False for command
+
+  GPIO.output(LCD_RS, mode) # RS
+
+  # High bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x10==0x10:
+    GPIO.output(LCD_D4, True)
+  if bits&0x20==0x20:
+    GPIO.output(LCD_D5, True)
+  if bits&0x40==0x40:
+    GPIO.output(LCD_D6, True)
+  if bits&0x80==0x80:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+  # Low bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x01==0x01:
+    GPIO.output(LCD_D4, True)
+  if bits&0x02==0x02:
+    GPIO.output(LCD_D5, True)
+  if bits&0x04==0x04:
+    GPIO.output(LCD_D6, True)
+  if bits&0x08==0x08:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+def lcd_toggle_enable():
+  # Toggle enable
+  time.sleep(E_DELAY)
+  GPIO.output(LCD_E, True)
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)
+  time.sleep(E_DELAY)
+
+def lcd_string(message,line):
+  # Cast to string
+  message = str(message)
+  # Send string to display
+  message = message.ljust(LCD_WIDTH," ")
+
+  lcd_byte(line, LCD_CMD)
+
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
+
+if __name__ == '__main__':
+
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    lcd_byte(0x01, LCD_CMD)
+    lcd_string("Goodbye!",LCD_LINE_1)
+    GPIO.cleanup()
